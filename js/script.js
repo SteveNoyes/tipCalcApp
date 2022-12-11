9854a270cbbe06ddcbd303c98768162364bc13ac
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-12-10 20:03:33 -0500

    js rubber duck

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Client enters bill total
+
+// Selects tip percentage 
+
+// Enters number of people 
+
+// bill x percentage = total tip amount
+
+// bill + tip = total 
+
+// tip / number of people = individual tip amount
+
+// total / number of people = individual total amount (including tip)
